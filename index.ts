09e245e598495b47086ea805ab446ec23fac48e2
psuwanarat365 <59331502+psuwanarat365@users.noreply.github.com>
2023-03-16 14:54:42 +0700

    S3-23: Synchronous emission - Next notification

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import { Observable } from 'rxjs';
 
 const observable$ = new Observable((subscriber) => {
   console.log('Observable executed');
+  subscriber.next('Alice');
 });
 
 console.log('before subscribe');
-observable$.subscribe();
+observable$.subscribe( value => console.log(value));
 console.log('after subscribe');
