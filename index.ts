3ec91c15e46a881db3937b9d2e43e21c515330f2
psuwanarat365 <59331502+psuwanarat365@users.noreply.github.com>
2023-03-16 15:08:43 +0700

    S3-25 Teardown - complete notification

--- index.ts
+++ index.ts
@@ -4,9 +4,20 @@ const observable$ = new Observable((subscriber) => {
   console.log('Observable executed');
   subscriber.next('Alice');
   subscriber.next('Ben')
-  setTimeout(()=> subscriber.next('Charlie'), 2000);
+  setTimeout(()=> {
+    subscriber.next('Charlie')
+    subscriber.complete();
+  }, 2000);
+
+  // teardown logic
+  return () => {
+    console.log('Teardown');
+  }
 });
 
 console.log('before subscribe');
-observable$.subscribe( value => console.log(value));
+observable$.subscribe({
+  next: value => console.log(value),
+  complete: ()=> console.log('Completed')
+});
 console.log('after subscribe');
