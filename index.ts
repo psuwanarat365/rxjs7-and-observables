50b815b5c59daa6ff6894f0f277b6a3408b11397
psuwanarat365 <59331502+psuwanarat365@users.noreply.github.com>
2023-03-18 21:12:39 +0700

    S5-39 fromEvent

--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
-import { from } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
 
-const somePromise = new Promise((resolve, reject) => {
-  // resolve('Resolved');
-  reject('Rejected!');
-});
+const triggerButton = document.querySelector('button#trigger');
+// const subscription = fromEvent<MouseEvent>(triggerButton,'click').subscribe(
+//   event => console.log(event.type, event.x, event.y)
+// );
+const triggerClick$ = new Observable<MouseEvent>(subscriber => {
+  const clickHandlerFn = event => {
+    console.log('Event callback executed')
+    subscriber.next(event)
+  }
+  triggerButton.addEventListener('click',clickHandlerFn);
 
-const observableFromPromise$ = from(somePromise);
+  return () => {
+    triggerButton.removeEventListener('click', clickHandlerFn)
+  }
+})
 
-observableFromPromise$.subscribe({
-  next: (value) => console.log(value),
-  error: (err) => console.log('Error', err),
-  complete: () => console.log('Completed'),
-});
+const subscription =triggerClick$.subscribe(
+  event => console.log(event.type, event.x, event.y)
+)
+
+setTimeout(()=> {
+  console.log('Unsubscribe');
+  subscription.unsubscribe();
+}, 5000)
